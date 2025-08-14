f554324b01d6690fe546396e5c779080e018257e
Diego Yon <diegoyon@outlook.com>
2025-08-14 17:45:30 -0500

    add icons to features

---
+++ src/data/features.js
@@ -0,0 +1,40 @@
+const features = [
+  {
+    icon: "FiAward",
+    title: "Professional CFIs",
+    description:
+      "Our Certified Flight Instructors (CFIs) are experienced and dedicated to helping you achieve your aviation goals.",
+  },
+  {
+    icon: "FiMonitor",
+    title: "Modern Cabri G2 Helicopters",
+    description:
+      "We use modern Cabri G2 helicopters for training, ensuring a safe and effective learning experience.",
+  },
+  {
+    icon: "FiClock",
+    title: "Flexible Training Options",
+    description:
+      "We offer flexible training options, including on-site student housing and mobile training at private airports or residences.",
+  },
+  {
+    icon: "FiMap",
+    title: "Helicopter Rentals",
+    description:
+      "We offer helicopter rentals for those looking to gain more flight experience or for personal use.",
+  },
+  {
+    icon: "FiLoader",
+    title: "External Load Instruction",
+    description:
+      "Our external load instruction prepares you for specialized flying tasks, enhancing your skill set.",
+  },
+  {
+    icon: "FiAirplay",
+    title: "Discovery Flights",
+    description:
+      "We offer discovery flights for those interested in experiencing the thrill of flying a helicopter.",
+  },
+];
+
+export default features;
