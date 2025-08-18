64892c2c19c47bc38dcda06725f344dc00cb4e78
Diego Yon <diegoyon@outlook.com>
2025-08-18 15:06:57 -0500

    add courses carrousel

---
+++ src/components/TrainingCourseCard.jsx
@@ -0,0 +1,46 @@
+import { FaArrowRight } from "react-icons/fa6";
+import { MdAccessTime } from "react-icons/md";
+
+const TrainingCourseCard = ({
+  name,
+  duration,
+  description,
+  button,
+  href,
+  image,
+}) => {
+  return (
+    <div className="shadow-primary-500/30 flex h-full flex-col overflow-hidden rounded-lg shadow-lg">
+      <div className="h-62 min-h-62">
+        <img
+          src={image.src}
+          alt={name}
+          className="h-full w-full object-cover"
+        />
+      </div>
+      <div className="flex h-full flex-col justify-between gap-5 p-6">
+        <div className="flex flex-col gap-3">
+          <h2 className="font-display text-2xl font-bold">{name}</h2>
+          <p className="flex items-baseline gap-2 font-semibold">
+            <MdAccessTime className="text-primary-500 size-6 translate-y-1" />
+            <span>{duration}</span>
+          </p>
+          <hr className="text-gray-300" />
+          <p className="paragraph text-accent-400 text-base font-medium">
+            {description}
+          </p>
+        </div>
+
+        <a
+          href={href}
+          className="btn-secondary text-primary-500! border-primary-500! group flex border-t px-5! whitespace-nowrap hover:text-white!"
+        >
+          Enroll Now
+          <FaArrowRight className="text-primary-500 duration-500 group-hover:text-white" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default TrainingCourseCard;
---
+++ src/components/TrainingCoursesCarrousel.jsx
@@ -0,0 +1,87 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import TrainingCourseCard from "./TrainingCourseCard";
+import privatePilotImage from "../assets/private-pilot-at-truflight-academy-dallas.avif";
+import instrumentRatingImage from "../assets/instrument-rating-truflight-academy.avif";
+import commercialPilotImage from "../assets/commercial-pilot-at-truflight-academy-dallas.avif";
+import slingLoadImage from "../assets/sling-load-training-truflight-academy-dallas.avif";
+
+const TrainingCoursesCarrousel = () => {
+  const courses = [
+    {
+      name: "Private Pilot",
+      duration: "6 months to 1 year",
+      description:
+        "The private pilot certificate is the first certification you will receive as a pilot.  You will be granted the privileges of flying a helicopter as the pilot in command and can fly passengers.",
+      button: "Become a private pilot",
+      href: "/flight-training/private-pilot",
+      image: privatePilotImage,
+    },
+    {
+      name: "Instrument Pilot",
+      duration: "6 months to 1 year",
+      description:
+        "Being an instrument rated pilot means that you've received training to fly a helicopter with sole reference to the flight instruments.  This allows you to safely navigate more challenging weather and makes you a sharper pilot.",
+      button: "Become instrument rated",
+      href: "/flight-training/instrument-rating",
+      image: instrumentRatingImage,
+    },
+    {
+      name: "Commercial Pilot",
+      duration: "3 to 6 months",
+      description:
+        "Obtaining a commercial pilot certificate allows you to fly for compensation or hire. Start your pilot career and gain flight experience while getting paid to fly.",
+      button: "Start your pilot career",
+      href: "/flight-training/commercial-pilot",
+      image: commercialPilotImage,
+    },
+    {
+      name: "Sling Load Training",
+      duration: "2 weeks",
+      description:
+        "Learn how to safely and effectively transport cargo using a helicopter.  Sling load training is essential for pilots who want to work in utility, firefighting, or search and rescue operations.",
+      button: "Become a CFI",
+      href: "/flight-training/flight-instructor",
+      image: slingLoadImage,
+    },
+  ];
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={50}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: true,
+        }}
+        loop={true}
+        modules={[Pagination, Autoplay]}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+        }}
+        className="mySwiper px-1!"
+      >
+        {courses.map((course, index) => (
+          <SwiperSlide key={index}>
+            <TrainingCourseCard {...course} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
+
+export default TrainingCoursesCarrousel;
