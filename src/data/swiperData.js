b18e33966c230acd657a43b3366885af4c488a42
Diego Yon <diegoyon@outlook.com>
2025-08-24 01:03:27 -0500

    update footer

--- src/data/swiperData.js
+++ src/data/swiperData.js
@@ -1,6 +1,7 @@
 const swiperData = [
   {
     imagePath: "/src/assets/discovery-flight-at-truflight-academy-dallas.avif",
+    imageAlt: "Helicopter flying over Dallas skyline",
     title: "Take Off with the Best Helicopter Flight School in Dallas",
     subtitle:
       "Looking for a helicopter flight school in Dallas, TX? Discover the best training at TruFlight Academy. Start your journey to becoming a skilled helicopter pilot today!",
@@ -9,6 +10,7 @@ const swiperData = [
   },
   {
     imagePath: "/src/assets/truflight-academy-chief-pilot.avif",
+    imageAlt: "Chief Pilot Todd Guison",
     title: "Our Chief Pilot at TruFlight Academy",
     subtitle:
       "Learn more about our expierienced Chief Pilot Todd Guison and his dedication to providing top-notch helicopter flight training at TruFlight Academy.",
@@ -17,6 +19,7 @@ const swiperData = [
   },
   {
     imagePath: "/src/assets/helicopter-at-truflight-academy.avif",
+    imageAlt: "Helicopter at TruFlight Academy",
     title: "Book a Discovery Flight at TruFlight Academy",
     subtitle:
       "Do you live in Dallas, TX and want to experience the thrill of flying a helicopter? Book a Discovery Flight and take the first step towards becoming a skilled helicopter pilot!",
@@ -25,7 +28,8 @@ const swiperData = [
   },
   {
     imagePath: "/src/assets/helicopter-controls.avif",
-    title: "First training helicopter Cabri G2 with AC to hit US soil!",
+    imageAlt: "Helicopter AC Controls",
+    title: "First Training Helicopter Cabri G2 with AC to Hit US Soil!",
     subtitle:
       "TruFlight Academy is proud to be the first helicopter flight school in the US to offer training on the Cabri G2 with the latest in avionics technology.",
     btnText: "Rent Now",
@@ -34,6 +38,7 @@ const swiperData = [
   {
     imagePath:
       "/src/assets/cabri-g2-helicopter-truflight-academy-dallas-texas.avif",
+    imageAlt: "Cabri G2 Helicopter at TruFlight Academy",
     title: "Take Your Training to the Next Step with External Load Training",
     subtitle:
       "Ready to take your helicopter training to the next level? Enroll in our External Load Training program at TruFlight Academy and learn how to safely and effectively operate helicopters with external loads.",
@@ -42,6 +47,7 @@ const swiperData = [
   },
   {
     imagePath: "/src/assets/sling-load-training-at-truflight-academy.avif",
+    imageAlt: "Sling Load Training at TruFlight Academy",
     title: "Looking for Sling Load Training? Look No Further!",
     subtitle:
       "TruFlight Academy offers top-notch Sling Load Training for aspiring helicopter pilots. Our experienced instructors will teach you the skills and techniques needed to safely and effectively operate helicopters with sling loads.",
