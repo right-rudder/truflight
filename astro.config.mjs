ace6e324517f326ee94d6bf23781b95fda8450d4
Diego Yon <diegoyon@outlook.com>
2025-09-08 16:01:27 -0500

    add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,4 +11,20 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  redirects: {
+    "/pricing": "/about/pricing",
+    "/careers": "/why-become-a-pilot",
+    "/wecometoyou": "/#weComeToYou",
+    "/registration": "/discovery-flight",
+    "/videos": "/",
+    "/book-now": "/about/pricing",
+    "/add-ons": "/add-on/private-helicopter-add-on",
+    "/about-us": "/about/our-story",
+    "/services-9": "/pilot-training/external-load-training",
+    "/about-the-pilot": "/about/our-chief-pilot",
+    "/news": "/blog",
+    "/pilot-training-old": "/pilot-training/private-pilot",
+    "/slingloadtraining": "/pilot-training/external-load-training",
+    "/general-9-1": "/pilot-training/external-load-training",
+  },
 });
