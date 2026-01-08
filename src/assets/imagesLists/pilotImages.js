755d247a4275f10caa350e5315c16f23a175abe6
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2026-01-08 18:39:04 -0300

    update image lists

--- src/assets/imagesLists/pilotImages.js
+++ src/assets/imagesLists/pilotImages.js
@@ -1,18 +1,18 @@
 export const pilotImages = [
   {
-    src: "src/assets/commercial-pilot-student-at-truflight-academy.jpg",
+    src: "/src/assets/commercial-pilot-student-at-truflight-academy.jpg",
     alt: "Commercial pilot student at TruFlight Academy",
   },
   {
-    src: "src/assets/external-load-operations-truflight-dallas.jpg",
+    src: "/src/assets/external-load-operations-truflight-dallas.jpg",
     alt: "External load operations at TruFlight in Dallas",
   },
   {
-    src: "src/assets/external-load-student-truflight-academy.jpg",
+    src: "/src/assets/external-load-student-truflight-academy.jpg",
     alt: "External load student at TruFlight Academy",
   },
   {
-    src: "src/assets/external-load-training-at-truflight-academy.jpg",
+    src: "/src/assets/external-load-training-at-truflight-academy.jpg",
     alt: "External load training at TruFlight Academy",
   },
 ];
