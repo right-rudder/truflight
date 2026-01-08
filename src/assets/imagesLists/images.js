72935ddbafe342264897fc4d05125087ec40110a
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2026-01-08 17:34:05 -0300

    Create images.js

---
+++ src/assets/imagesLists/images.js
@@ -0,0 +1,374 @@
+const images = [
+  {
+    src: "src/assets/airplane.svg",
+    alt: "Airplane icon",
+  },
+  {
+    src: "src/assets/baby-guison.avif",
+    alt: "Baby Guison",
+  },
+  {
+    src: "src/assets/blob-haikei.svg",
+    alt: "Blob haikei background",
+  },
+  {
+    src: "src/assets/bottom-view-from-truflight-helicopter.jpg",
+    alt: "Bottom view from TruFlight helicopter",
+  },
+  {
+    src: "src/assets/cabri-g2-helicopter-truflight-academy-dallas-texas.avif",
+    alt: "Cabri G2 helicopter at TruFlight Academy in Dallas, Texas",
+  },
+  {
+    src: "src/assets/cabri-panel-truflight.jpg",
+    alt: "Cabri helicopter instrument panel at TruFlight",
+  },
+  {
+    src: "src/assets/certified-flight-instructor-add-on-at-truflight-academy.jpg",
+    alt: "Certified Flight Instructor add-on at TruFlight Academy",
+  },
+  {
+    src: "src/assets/certified-flight-instructor-truflight-academy.jpg",
+    alt: "Certified Flight Instructor at TruFlight Academy",
+  },
+  {
+    src: "src/assets/chief-pilot-guison-army.avif",
+    alt: "Chief Pilot Guison in Army",
+  },
+  {
+    src: "src/assets/chief-pilot-todd-guison-at-truflight-academy.avif",
+    alt: "Chief Pilot Todd Guison at TruFlight Academy",
+  },
+  {
+    src: "src/assets/chief-pilot-todd-guison-checking-helicopter.jpg",
+    alt: "Chief Pilot Todd Guison checking helicopter",
+  },
+  {
+    src: "src/assets/chief-pilot-todd-guison.jpg",
+    alt: "Chief Pilot Todd Guison",
+  },
+  {
+    src: "src/assets/commercial-flight-truflight-academy.jpg",
+    alt: "Commercial flight training at TruFlight Academy",
+  },
+  {
+    src: "src/assets/commercial-helicopter-flight-training-truflight.jpg",
+    alt: "Commercial helicopter flight training at TruFlight",
+  },
+  {
+    src: "src/assets/commercial-pilot-at-truflight-academy-dallas.avif",
+    alt: "Commercial pilot at TruFlight Academy in Dallas",
+  },
+  {
+    src: "src/assets/commercial-pilot-student-at-truflight-academy.jpg",
+    alt: "Commercial pilot student at TruFlight Academy",
+  },
+  {
+    src: "src/assets/customer-taking-a-discovery-flight-at-truflight-academy.jpg",
+    alt: "Customer taking a discovery flight at TruFlight Academy",
+  },
+  {
+    src: "src/assets/discovery-flight-at-truflight-academy-dallas.avif",
+    alt: "Discovery flight at TruFlight Academy in Dallas",
+  },
+  {
+    src: "src/assets/discovery-flight-at-truflight-academy-dallas.jpg",
+    alt: "Discovery flight at TruFlight Academy in Dallas",
+  },
+  {
+    src: "src/assets/discovery-flight-fun-at-truflight-academy.jpg",
+    alt: "Discovery flight fun at TruFlight Academy",
+  },
+  {
+    src: "src/assets/discovery-flight-helicopter-truflight-academy.jpg",
+    alt: "Discovery flight helicopter at TruFlight Academy",
+  },
+  {
+    src: "src/assets/discovery-flight.svg",
+    alt: "Discovery flight icon",
+  },
+  {
+    src: "src/assets/external-load-operations-truflight-dallas.jpg",
+    alt: "External load operations at TruFlight in Dallas",
+  },
+  {
+    src: "src/assets/external-load-student-truflight-academy.jpg",
+    alt: "External load student at TruFlight Academy",
+  },
+  {
+    src: "src/assets/external-load-training-at-truflight-academy.jpg",
+    alt: "External load training at TruFlight Academy",
+  },
+  {
+    src: "src/assets/external-load-training-in-dallas-texas.jpg",
+    alt: "External load training in Dallas, Texas",
+  },
+  {
+    src: "src/assets/external-load.svg",
+    alt: "External load icon",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-flying-over-dallas-texas.jpg",
+    alt: "Guimbal Cabri G2 flying over Dallas, Texas",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-in-dallas-texas.jpg",
+    alt: "Guimbal Cabri G2 in Dallas, Texas",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy-spec1.jpg",
+    alt: "Guimbal Cabri G2 specifications at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy-spec2.jpg",
+    alt: "Guimbal Cabri G2 specifications at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy-spec3.jpg",
+    alt: "Guimbal Cabri G2 specifications at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy-spec4.jpg",
+    alt: "Guimbal Cabri G2 specifications at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy-spec5.jpg",
+    alt: "Guimbal Cabri G2 specifications at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy-spec6.jpg",
+    alt: "Guimbal Cabri G2 specifications at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy-spec7.jpg",
+    alt: "Guimbal Cabri G2 specifications at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy-spec8.jpg",
+    alt: "Guimbal Cabri G2 specifications at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guimbal-cabri-g2-truflight-academy.jpg",
+    alt: "Guimbal Cabri G2 at TruFlight Academy",
+  },
+  {
+    src: "src/assets/guison-family-at-expo.avif",
+    alt: "Guison family at expo",
+  },
+  {
+    src: "src/assets/guison-family-inside-helicopter.jpg",
+    alt: "Guison family inside helicopter",
+  },
+  {
+    src: "src/assets/guison-family-kids.avif",
+    alt: "Guison family with kids",
+  },
+  {
+    src: "src/assets/helicopter-agriculture.jpg",
+    alt: "Helicopter agriculture application",
+  },
+  {
+    src: "src/assets/helicopter-at-truflight-academy.avif",
+    alt: "Helicopter at TruFlight Academy",
+  },
+  {
+    src: "src/assets/helicopter-blackhawk-truflight-academy.avif",
+    alt: "Blackhawk helicopter at TruFlight Academy",
+  },
+  {
+    src: "src/assets/helicopter-cabri-drifting-dallas-texas.jpg",
+    alt: "Cabri helicopter drifting over Dallas, Texas",
+  },
+  {
+    src: "src/assets/helicopter-cherry-drying.jpg",
+    alt: "Helicopter cherry drying application",
+  },
+  {
+    src: "src/assets/helicopter-commercial-external-load-truflight.jpg",
+    alt: "Commercial helicopter external load at TruFlight",
+  },
+  {
+    src: "src/assets/helicopter-controls.avif",
+    alt: "Helicopter controls",
+  },
+  {
+    src: "src/assets/helicopter-corporate.jpg",
+    alt: "Corporate helicopter",
+  },
+  {
+    src: "src/assets/helicopter-firefighter.jpg",
+    alt: "Firefighter helicopter",
+  },
+  {
+    src: "src/assets/helicopter-lifeflight.jpg",
+    alt: "Lifeflight medical helicopter",
+  },
+  {
+    src: "src/assets/helicopter-military.jpg",
+    alt: "Military helicopter",
+  },
+  {
+    src: "src/assets/helicopter-more-applications.jpg",
+    alt: "More helicopter applications",
+  },
+  {
+    src: "src/assets/helicopter-oil-rig-transport.jpg",
+    alt: "Oil rig transport helicopter",
+  },
+  {
+    src: "src/assets/helicopter-police.jpg",
+    alt: "Police helicopter",
+  },
+  {
+    src: "src/assets/helicopter-powerline-surveying.jpg",
+    alt: "Powerline surveying helicopter",
+  },
+  {
+    src: "src/assets/helicopter-taking-off-at-truflight-academy.avif",
+    alt: "Helicopter taking off at TruFlight Academy",
+  },
+  {
+    src: "src/assets/helicopter-taking-off-at-truflight.jpg",
+    alt: "Helicopter taking off at TruFlight",
+  },
+  {
+    src: "src/assets/helicopter-tour.jpg",
+    alt: "Helicopter tour",
+  },
+  {
+    src: "src/assets/helicopters-army-truflight-aviation.avif",
+    alt: "Army helicopters at TruFlight Aviation",
+  },
+  {
+    src: "src/assets/instrument-rating-truflight-academy.avif",
+    alt: "Instrument rating at TruFlight Academy",
+  },
+  {
+    src: "src/assets/owner-of-truflight-academy-scarlett-guison.avif",
+    alt: "Owner of TruFlight Academy Scarlett Guison",
+  },
+  {
+    src: "src/assets/pilot-training.svg",
+    alt: "Pilot training icon",
+  },
+  {
+    src: "src/assets/private-pilot-at-truflight-academy-dallas.avif",
+    alt: "Private pilot at TruFlight Academy in Dallas",
+  },
+  {
+    src: "src/assets/private-pilot-training-at-truflight.jpg",
+    alt: "Private pilot training at TruFlight",
+  },
+  {
+    src: "src/assets/scarlett-and-todd-guison.avif",
+    alt: "Scarlett and Todd Guison",
+  },
+  {
+    src: "src/assets/sling-load-training-at-truflight-academy.avif",
+    alt: "Sling load training at TruFlight Academy",
+  },
+  {
+    src: "src/assets/sling-load-training-truflight-academy-dallas.avif",
+    alt: "Sling load training at TruFlight Academy in Dallas",
+  },
+  {
+    src: "src/assets/SPT_4938.jpg",
+    alt: "Helicopter flight photo SPT_4938",
+  },
+  {
+    src: "src/assets/student-pilot-at-truflight-academy-dallas.jpg",
+    alt: "Student pilot at TruFlight Academy in Dallas",
+  },
+  {
+    src: "src/assets/the-guison-family.avif",
+    alt: "The Guison family",
+  },
+  {
+    src: "src/assets/todd-and-scarlett-guison-at-heliexpo.avif",
+    alt: "Todd and Scarlett Guison at HeliExpo",
+  },
+  {
+    src: "src/assets/todd-and-scarlett-guison-wedding.avif",
+    alt: "Todd and Scarlett Guison wedding",
+  },
+  {
+    src: "src/assets/todd-guison-and-scarlett-guison.avif",
+    alt: "Todd Guison and Scarlett Guison",
+  },
+  {
+    src: "src/assets/todd-guison-army.avif",
+    alt: "Todd Guison in Army",
+  },
+  {
+    src: "src/assets/todd-guison-in-helicopter.avif",
+    alt: "Todd Guison in helicopter",
+  },
+  {
+    src: "src/assets/todd-guison-in-truflight-academy.avif",
+    alt: "Todd Guison in TruFlight Academy",
+  },
+  {
+    src: "src/assets/truflight-academy-chief-pilot.avif",
+    alt: "TruFlight Academy Chief Pilot",
+  },
+  {
+    src: "src/assets/truflight-academy-helicopter-in-hangar.jpg",
+    alt: "TruFlight Academy helicopter in hangar",
+  },
+  {
+    src: "src/assets/truflight-academy-present-at-heliexpo.avif",
+    alt: "TruFlight Academy present at HeliExpo",
+  },
+  {
+    src: "src/assets/truflight-at-dallas-airport-commercial-pilot.jpg",
+    alt: "TruFlight at Dallas airport commercial pilot",
+  },
+  {
+    src: "src/assets/truflight-helicopter-dawn-takeoff.jpg",
+    alt: "TruFlight helicopter dawn takeoff",
+  },
+  {
+    src: "src/assets/truflight-helicopter-flying-over-dallas-texas.jpg",
+    alt: "TruFlight helicopter flying over Dallas, Texas",
+  },
+  {
+    src: "src/assets/truflight-helicopter-in-dallas.avif",
+    alt: "TruFlight helicopter in Dallas",
+  },
+  {
+    src: "src/assets/truflight-helicopter-in-the-sunset.jpg",
+    alt: "TruFlight helicopter in the sunset",
+  },
+  {
+    src: "src/assets/truflight-helicopter-interior.avif",
+    alt: "TruFlight helicopter interior",
+  },
+  {
+    src: "src/assets/truflight-helicopter.png",
+    alt: "TruFlight helicopter",
+  },
+  {
+    src: "src/assets/truflight-instructor-with-student-on-helicopter.jpg",
+    alt: "TruFlight instructor with student on helicopter",
+  },
+  {
+    src: "src/assets/truflight-logo.avif",
+    alt: "TruFlight logo",
+  },
+  {
+    src: "src/assets/Truflight-OG.png",
+    alt: "TruFlight original logo",
+  },
+  {
+    src: "src/assets/truflight-student-finishing-helicopter-program.jpg",
+    alt: "TruFlight student finishing helicopter program",
+  },
+  {
+    src: "src/assets/trulflight-cabri-helicopter-in-the-sunset.jpg",
+    alt: "TruFlight Cabri helicopter in the sunset",
+  },
+  {
+    src: "src/assets/trulflight-helicopter-in-dallas-airport.jpg",
+    alt: "TruFlight helicopter in Dallas airport",
+  },
+];
