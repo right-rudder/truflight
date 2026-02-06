417203bf7d996461630cc15084e8ad0c8af8980d
GuiJRNunes <116040992+GuiJRNunes@users.noreply.github.com>
2026-02-06 18:17:33 -0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,12 @@
 export const SITE_TITLE = "TruFlight Academy";
 export const SITE_DESCRIPTION =
   "Looking for a helicopter flight school in Dallas, TX? Discover the best training at TruFlight Academy. Start your journey to becoming a skilled helicopter pilot today!";
+  
 export const PHONE_NUMBER = "+1-833-722-iFLY";
 export const INSTAGRAM_URL = "https://www.instagram.com/truflight.academy/";
 export const FACEBOOK_URL = "https://www.facebook.com/truflightacademy";
+
+export const ADDRESS = "9007 FM 902, Howe, TX 75459";
 export const ADDRESS_LINE_1 = "9007 FM 902";
 export const ADDRESS_LINE_2 = "Howe, TX 75459";
 export const AIRPORT = "Finney Field Airport (T47)";
