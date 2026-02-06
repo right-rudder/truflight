2bb4e144af4813969942b37390cb3e812b687739
GuiJRNunes <116040992+GuiJRNunes@users.noreply.github.com>
2026-02-06 18:17:10 -0300

    Update the locations collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,7 +14,7 @@ const blog = defineCollection({
     }),
 });
 
-const flightTraining = defineCollection({
+const locationsCollection = defineCollection({
   // Load Markdown and MDX files in the `src/content/flight-training/` directory.
   loader: glob({
     base: "./src/content/flight-training",
@@ -26,12 +26,90 @@ const flightTraining = defineCollection({
       city: z.string(),
       stateShort: z.string(),
       stateLong: z.string(),
-      description: z.string(),
-      descriptions: z.array(z.string()),
-      airportLocation: z.string(),
-      distance: z.string(),
-      locationKeywords: z.array(z.string()),
+
+      meta: z.object({
+        description: z.string(),
+        keywords: z.string(),
+      }).optional(),
+
+      header: z.object({
+        title: z.string(),
+        subtitle: z.string(),
+        primaryImage: z.object({
+          src: z.string(),
+          alt: z.string(),
+        }),
+        button: z.object({
+          href: z.string(),
+          text: z.string(),
+        })
+      }).optional(),
+
+      whyUs: z.object({
+        title: z.string(),
+        features: z.array(z.object({
+          icon: z.string(),
+          title: z.string(),
+          description: z.string(),
+          link: z.string().optional(),
+        })),
+      }).optional(),
+
+      aboutUs: z.object({
+        upperHeader: z.string(),
+        title: z.string(),
+        description: z.string().optional(),
+        bulletPoints: z.array(z.string()).optional(),
+        secondDescription: z.string().optional(),
+        cta: z.object({
+          text: z.string(),
+          url: z.string(),
+        }),
+        image: z.object({
+          src: z.string(),
+          alt: z.string(),
+        }),
+      }).optional(),
+
+      weComeToYou: z.object({
+        upperHeader: z.string(),
+        title: z.string(),
+        description: z.string(),
+        cta: z.object({
+          text: z.string(),
+          url: z.string(),
+        }),
+        image: z.object({
+          src: z.string(),
+          alt: z.string(),
+        }),
+      }).optional(),
+
+      googleMaps: z.object({
+        upperHeader: z.string(),
+        title: z.string(),
+        description: z.string(),
+        mapsLink: z.string(),
+      }).optional(),
+
+      faqs: z.array(z.object({
+        question: z.string(),
+        answer: z.string(),
+      })).optional(),
+
+      finalCTA: z.object({
+        title: z.string(),
+        description: z.string(),
+        ctas: z.array(z.object({
+          text: z.string(),
+          url: z.string(),
+        })),
+        image: z.object({
+          src: z.string(),
+          alt: z.string(),
+        }),
+      }).optional(),
     }),
 });
 
-export const collections = { blog: blog, "flight-training": flightTraining };
+export const collections = { blog: blog, "flight-training": locationsCollection };
